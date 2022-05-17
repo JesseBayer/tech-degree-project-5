3e0f624d9369f624ea383f652d8174ffb9a31f3f
Jesse Bayer <jessebayer53@gmail.com>
2022-05-17 18:02:07 -0500

    Search error

--- js/script.js
+++ js/script.js
@@ -1,21 +1,22 @@
 let userInput = document.getElementById("search"); 
 
-userInput.addEventListener("keyup", searchFunction); 
+// userInput.addEventListener("keyup", searchFunction); 
 
 function searchFunction() {
     var i = document.getElementById("search");
     i.value = i.value.toLowerCase();
-    console.log("search");
 
-    let images = document.querySelector(".container").children;
 
-    for (i = 0; i < images.length; i++) {
-        if (images.getAttribute("data-caption").toLowerCase().includes(userInput)) {
-            images.style.display = "none";
+    let image = document.querySelector(".container").children;
+
+    for (i = 0; i < image.length; i++) {
+        if (image[i].getAttribute("data-caption").toLowerCase().includes(userInput)) {
+            image[i].style.display = "";
         }
             else {
-                images.style.display = "hidden";
+                image[i].style.display = "hidden";
             }
         }
     }
 
+    userInput.addEventListener("keyup", searchFunction); 
