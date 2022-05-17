aa64ec2f58b3cc8c0f5981b612b5d3c6510254c9
Jesse Bayer <jessebayer53@gmail.com>
2022-05-17 16:53:53 -0500

    getAttribute error

--- js/script.js
+++ js/script.js
@@ -1,10 +1,21 @@
-let userInput = document.getElementsByID("search"); 
+let userInput = document.getElementById("search"); 
 
-userInput.addEventListener("keyup", searchFunction);
+userInput.addEventListener("keyup", searchFunction); 
 
 function searchFunction() {
-    var i = document.getElementsByID("search")
+    var i = document.getElementById("search");
     i.value = i.value.toLowerCase();
-    console.log("search")
-}
+    console.log("search");
+
+    let images = document.querySelector(".container").children;
+
+    for (i = 0; i < images.length; i++) {
+        if (images.getAttribute("data-caption").toLowerCase().includes(userInput)) {
+            images.style.display = "none";
+        }
+            else {
+                images.style.display = "hidden";
+            }
+        }
+    }
 
