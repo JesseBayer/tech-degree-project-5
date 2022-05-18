d043ea3c2e42b944ab7727d265532315c0b7819d
Jesse Bayer <jessebayer53@gmail.com>
2022-05-17 21:09:52 -0500

    js fixed

--- js/script.js
+++ js/script.js
@@ -1,20 +1,25 @@
+//Asigns the input from the search bar to userInput
 let userInput = document.getElementById("search"); 
 
-// userInput.addEventListener("keyup", searchFunction); 
-
 function searchFunction() {
-    var i = document.getElementById("search");
-    i.value = i.value.toLowerCase();
+
+    //Sets the value of userInput to userSearch and sets it to lower case
+    let userSearch = userInput.value.toLowerCase();
 
 
     let image = document.querySelector(".container").children;
 
     for (i = 0; i < image.length; i++) {
-        if (image[i].getAttribute("data-caption").toLowerCase().includes(userInput)) {
+
+        //Sets the value of data-caption to image_caption and sets it to lower case
+        let image_caption = image[i].getAttribute("data-caption").toLowerCase();
+
+        //Conditional to check if the image caption includes the user input
+        if(image_caption.includes(userSearch)) {
             image[i].style.display = "";
         }
             else {
-                image[i].style.display = "hidden";
+                image[i].style.display = "none";
             }
         }
     }
